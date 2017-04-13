11a1c2417086e1397af584e04a19ede05a9d5dae
LittleDev <nicolas.gaillard@viacesi.fr>
2017-04-13 17:14:44 +0200

    Added 'pictures' table migration

---
+++ database/migrations/2017_04_13_150740_create_pictures_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_activities');
+            $table->integer('id_users');
+            $table->foreign('id_activities')->references('id')->on('activities');
+            $table->foreign('id_users')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pictures');
+    }
+}
