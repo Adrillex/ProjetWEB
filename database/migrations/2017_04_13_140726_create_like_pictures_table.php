af70196b5df2bf4239f5d3abd83cae9c4203783a
Unknown <alexandre.courallet@viacesi.fr>
2017-04-13 16:14:15 +0200

    Added migration class for like_picture table

---
+++ database/migrations/2017_04_13_140726_create_like_pictures_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikePicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('like_pictures', function (Blueprint $table) {
+            $table->integer('id_user')->unsigned();
+            $table->integer('id_picture')->unsigned();
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('id_picture')->references('id')->on('pictures')->onDelete('cascade');
+            $table->primary(['id_user', 'id_picture']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('like_pictures');
+    }
+}
