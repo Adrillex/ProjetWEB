e3aad8aae4745faf13d52f955fe5312253738f14
Unknown <alexandre.courallet@viacesi.fr>
2017-04-13 13:48:18 +0200

    Activity table added

---
+++ database/migrations/2017_04_13_110128_create_activities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->float('price');
+            $table->integer('id_users')->unsigned();
+            $table->foreign('id_users')->references('id')->on('news')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
