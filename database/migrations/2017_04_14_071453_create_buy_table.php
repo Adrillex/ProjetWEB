b26d4f89ede8cfb80446fdf1981a49fa6fdefea7
Unknown <aplbeck@gmail.com>
2017-04-14 09:25:57 +0200

    added buy table
    done

---
+++ database/migrations/2017_04_14_071453_create_buy_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBuyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buy', function (Blueprint $table) {
+            $table->integer('id');
+            $table->integer('id_user')->unsigned();
+            $table->integer('id_product')->unsigned();
+            $table->foreign('id_user')->reference('id')->on('users')->ondelete('cascade');
+            $table->foreign('id_product')->reference('id')->on('products')->ondelete('cascade');
+
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buy');
+    }
+}
