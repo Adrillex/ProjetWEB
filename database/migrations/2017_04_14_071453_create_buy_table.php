f6aeaaf0659ac798b60645f51606e09821de36b9
Vincent Dugard <vincent.dugard@viacesi.fr>
2017-04-14 15:30:41 +0200

    Made some change with the migrations /2
    THX BECKY

--- database/migrations/2017_04_14_071453_create_buy_table.php
+++ database/migrations/2017_04_14_071453_create_buy_table.php
@@ -17,8 +17,8 @@ class CreateBuyTable extends Migration
             $table->integer('id');
             $table->integer('id_user')->unsigned();
             $table->integer('id_product')->unsigned();
-            $table->foreign('id_user')->reference('id')->on('users')->ondelete('cascade');
-            $table->foreign('id_product')->reference('id')->on('products')->ondelete('cascade');
+            $table->foreign('id_user')->references('id')->on('users')->ondelete('cascade');
+            $table->foreign('id_product')->references('id')->on('products')->ondelete('cascade');
             $table->primary(['id_user', 'id_product']);
             //$table->timestamps();
         });
