227f93cf193c8dffb77cee99f495744baf9a68ab
Vincent Dugard <vincent.dugard@viacesi.fr>
2017-04-14 20:06:44 +0200

    Added bde middleware

---
+++ app/Http/Middleware/Bde.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class Bde
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check and Auth::user()->status === 2){
+            return $next($request);
+        }
+        return redirect('home');
+    }
+}
