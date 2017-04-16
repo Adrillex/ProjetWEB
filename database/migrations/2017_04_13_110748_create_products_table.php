727da0f1167e13bb85b003b13c73fc2ea87add21
LittleDev <nicolas.gaillard@viacesi.fr>
2017-04-16 21:08:46 +0200

    Modification of products table and create new table for the pictures of the products

--- database/migrations/2017_04_13_110748_create_products_table.php
+++ database/migrations/2017_04_13_110748_create_products_table.php
@@ -20,6 +20,8 @@ class CreateProductsTable extends Migration
             $table->float('price');
             $table->integer('quantity');
             $table->timestamps();
+            $table->integer('id_categories')->unsigned();
+            $table->foreign('id_categories')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
---
+++ database/migrations/2017_04_16_190527_create_pictureProducts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePictureProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscribe', function (Blueprint $table) {
+            $table->integer('id_pictures')->unsigned();
+            $table->integer('id_products')->unsigned();
+            $table->foreign('id_pictures')->references('id')->on('pictures')->onDelete('cascade');
+            $table->foreign('id_products')->references('id')->on('products')->onDelete('cascade');
+            $table->primary(['id_pictures', 'id_products']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pictureProducts');
+    }
+}
