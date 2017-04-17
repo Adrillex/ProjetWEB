dc36ba8d43bfa67bbaddb6948b6591c96f84d18e
Unknown <aplbeck@gmail.com>
2017-04-17 13:07:10 +0200

    Initialization 'news' part
    done

---
+++ resources/views/news/index.blade.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alexandre
+ * Date: 17/04/2017
+ * Time: 12:43
+ */
+
+echo 'salutmaggle';
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/home', 'HomeController@index');
 
 Route::resource('/activities', 'ActivitiesController');
 Route::resource('/products', 'ProductsController');
+Route::resource('/news', 'NewsController');
